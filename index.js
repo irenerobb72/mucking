c6e58fbf4ff8be54e3409730967e3d7a2ffacce2
Aaron Robb <aaronrobb@Sugarat.local>
2015-12-28 09:28:31 +1300

    For work today

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = koa()
 const serve = require('koa-static')
 const server = require('http').createServer(app.callback()).listen(3000)
 const io = require('socket.io')(server)
+const users = require('./lib/users')
 
 app.use(serve(__dirname + '/client'))
 
@@ -12,15 +13,14 @@ const latest = []
 io.on('connection', (socket) => {
   sockets.push(socket)
   latestMessages(socket)
-
   socket.on('message', (data) => {
     latest.length > 15 ? latest.shift().push(socket.userName + ': ' + data) : latest.push(socket.userName + ': ' + data)
     broadcast('message', socket.userName + ': ' + data)
   })
 
   socket.on('newuser', (data) => {
-    socket.userName = data
-    broadcast('message', data + ' has joined the channel')
+    socket.user = users.createUser(data)
+    broadcast('message', socket.user.userName + ' has joined the channel')
   })
 })
 
