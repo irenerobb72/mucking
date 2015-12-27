dcdb75fba1d55116253d4cd102b18d2830ec5f34
Aaron Robb <email": null>
2015-12-28 12:57:35 +1300

    Blah

--- client/js/main.js
+++ client/js/main.js
@@ -2,25 +2,35 @@
 const socket = io.connect()
 
 let localUser
+let chat = document.getElementById('chat-box');
 
 socket.on('connect', function () {
+
   if (localUser) socket.emit('newuser', localUser)
+
   socket.on('message', function (data) {
     $('#chat-box').append('<p>' + data + '</p>')
+    chat.scrollTop = chat.scrollHeight;
+  })
+  socket.on('updateUsers', (data) => {
+    $('#users').text('')
+    data.forEach((name) => {
+      $('#users').append('<p>' + name + '</p>')
+    })
   })
 })
 
 //Chat Handler
 
-$('#chat-input').on('submit', (e) => {
+const chatInput = (e) => {
   e.preventDefault()
   if($('#chat-input input').val().length) {
     socket.emit('message', $('#chat-input input').val())
   }
   $('#chat-input input').val('')
-})
+}
 
-$('#user-login').on('submit', (e) => {
+const userLogin = (e) => {
   e.preventDefault()
   if ($('#user-login input').val().length) {
     localUser = $('#user-login input').val()
@@ -30,7 +40,12 @@ $('#user-login').on('submit', (e) => {
     $('#user-login input').css('border-color', 'red')
     $('#user-login').append('<p class="error">Please add a username scrub</p>')
   }
-})
+}
+
+
+$('#chat-input').on('submit', chatInput)
+
+$('#user-login').on('submit', userLogin)
 
 
 const hideLogin = () => {
--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+'use strict'
+
 const koa = require('koa')
 const app = koa()
 const serve = require('koa-static')
 const server = require('http').createServer(app.callback()).listen(3000)
 const io = require('socket.io')(server)
 const users = require('./lib/users')
+const R = require('ramda')
 
 app.use(serve(__dirname + '/client'))
 
 const sockets = []
 const latest = []
+let currentUsers = []
+
 
 io.on('connection', (socket) => {
   sockets.push(socket)
   latestMessages(socket)
+
   socket.on('message', (data) => {
-    latest.length > 15 ? latest.shift().push(socket.userName + ': ' + data) : latest.push(socket.userName + ': ' + data)
-    broadcast('message', socket.userName + ': ' + data)
+    if (latest.length > 15) latest.shift()
+    latest.push(socket.user.username + ': ' + data)
+    broadcast('message', socket.user.username + ': ' + data)
   })
 
   socket.on('newuser', (data) => {
     socket.user = users.createUser(data)
-    broadcast('message', socket.user.userName + ' has joined the channel')
+    broadcast('message', socket.user.username + ' has joined the channel')
+    currentUsers.push(socket.user.username)
+    updateUserList(currentUsers)
+  })
+
+  socket.on('disconnect', (data) => {
+    currentUsers = R.filter((user) => user != socket.user.username, currentUsers)
+    broadcast('message', socket.user.username + ' has left the channel')
+    updateUserList(currentUsers)
   })
 })
 
@@ -36,5 +51,11 @@ const latestMessages = (socket) => {
     socket.emit('message', message)
   })
 }
+
+const updateUserList = (data) => {
+  broadcast('updateUsers', data)
+}
+
+
 
 console.log('listening on port 3000')
